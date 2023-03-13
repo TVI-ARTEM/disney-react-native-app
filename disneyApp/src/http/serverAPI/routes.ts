3f822b9bcf25016bfe1eee27b8b165ce2a602379
TVI-ARTEM <azhulin@edu.hse.ru>
2023-03-13 12:06:06 +0300

    Fix API URL

--- disneyApp/src/http/serverAPI/routes.ts
+++ disneyApp/src/http/serverAPI/routes.ts
@@ -1,4 +1,4 @@
-export const API_URL = "http://192.168.1.59:5076"
+export const API_URL = "http://192.168.1.59:5075"
 export const API_USER_REGISTRATION = "api/user/registration"
 export const API_USER_LOGIN = "api/user/login"
 export const API_USER_LOGOUT = "api/user/logout"
